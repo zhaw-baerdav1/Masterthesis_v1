0eea5253e116e437277b6d4f30f29cd2597520eb
zhaw-baerdav1 <baerdav1@students.zhaw.ch>
2020-03-23 14:34:14 +0100

    reorganize setup to allow successful build

--- Assets/Scripts/CustomHeadRotationController.cs
+++ Assets/Scripts/CustomHeadRotationController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CustomHeadRotationController : MonoBehaviour
 {
 
-    public Eyes eyes;
+    //public Eyes eyes;
 
     // Start is called before the first frame update
     void Start()
